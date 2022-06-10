57046931d50acefe2b7726d99743c048d881da88
Edward-Precious Omegbu <eomegbu@gmail.com>
2022-06-10 11:02:52 +0100

    initial

---
+++ app.js
@@ -0,0 +1,122 @@
+let Hermes = [
+    {
+        displayName: "CodeGiyu",
+        name: "Edward-Precious Omegbu",
+        image: "img/giyu.png",
+        age: 26,
+        complexion: "Dark",
+        thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+        bestFood: "Pasta",
+        bestMovie: "One Piece"
+    },
+
+    {
+        displayName: "Natacha",
+        name: "Natacha Danbadan",
+        image: "img/nat.png",
+        age: "???",
+        complexion: "Black",
+        thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+        bestFood: "Amala and Beans",
+        bestMovie: "Friday The 13th"
+    },
+
+    // {
+    //     displayName: "Adedamola",
+    //     name: "Jimi-Bada Adedamola",
+    //     image: "img/bada.png",
+    //     age: 24,
+    //     complexion: "Dark",
+    //     thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    //     bestFood: "Beans!",
+    //     bestMovie: "Star Wars: Revenge of The Sith"
+    // },
+
+    {
+        displayName: "Fawsiyyah",
+        name: "Fawsiyyah Lamidi",
+        image: "img/zee.png",
+        age: 17,
+        complexion: "Fair?",
+        thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+        bestFood: "Rice and Beans with Stew, Egg and Dodo",
+        bestMovie: "Little"
+    }
+
+    // {
+    //     displayName: "DeeVeee",
+    //     name: "Obi Divine",
+    //     image: "img/divi.png",
+    //     age: 22,
+    //     complexion: "Dark",
+    //     thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    //     bestFood: "Yam and Egg Sauce",
+    //     bestMovie: "Ready Player One"
+    // },
+
+    // {
+    //     displayName: "Ifechukwude",
+    //     name: "Ifechukwude Nwaodor",
+    //     image: "img/ify.png",
+    //     age: "??",
+    //     complexion: "Fair",
+    //     thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    //     bestFood: "Rice",
+    //     bestMovie: "Titanic"
+    // },
+
+    // {
+    //     displayName: "Ayordeji",
+    //     name: "Ayordeji Eluyemi",
+    //     image: "img/ayor.png",
+    //     age: 26,
+    //     complexion: "dark",
+    //     thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    //     bestFood: "Fried Rice",
+    //     bestMovie: "Arthdal Chronicles"
+    // },
+
+    // {
+    //     displayName: "Michael",
+    //     name: "Okike Chinonso Michael",
+    //     image: "img/mike.png",
+    //     age: "??",
+    //     complexion: "Dark",
+    //     thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    //     bestFood: "?????",
+    //     bestMovie: "?????"
+    // },
+
+    // {
+    //     displayName: "Sisolis Wayne",
+    //     name: "Akorede Mohammed",
+    //     image: "img/wayne.png",
+    //     age: "??",
+    //     complexion: "Dark",
+    //     thingsLearned: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    //     bestFood: "?????",
+    //     bestMovie: "??????"
+    // },
+];
+
+// let row = document.getElementById("row");
+
+// for (i = 0; i < Hermes.length; i++) {
+//     let card = `<div class="col-4 px-3 mb-4 border">
+//                 <div class="card p-3">
+//                     <div class="img-container ratio ratio-1x1">
+//                         <img src="${Hermes[i].image}" alt="" class="img-fluid">
+//                     </div>
+//                     <p class="name">${Hermes[i].name}</p>
+//                     <button class="alert" onclick="nameAlert(${i})">Click me!</button>
+//                 </div>
+//             </div>`;
+
+//     row.innerHTML += card;
+
+    
+// }
+
+// function nameAlert(i) {
+//     alert(Hermes[i].name);
+// }
